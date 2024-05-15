522aef4870c4610f2f6e504e0b9b3ad61ad4aba8
Park-GeunWoo <pcdpcd100@gmail.com>
2024-05-15 18:08:34 +0900

    test

---
+++ Test/Main.java
@@ -0,0 +1,19 @@
+package Test;
+
+//클래스
+//필드
+//메소드
+//캡슐화
+//상속
+//다양성
+//오버로딩 = 이름이같은 메소드로 다른 매개변수
+//오버라이딩 = 같은 메소드 재정의
+//this = 다른객체의 필드를 구별하기 위함
+//private 동일한 클래스 내에서만 사용가능
+//this()
+public class Main {
+
+    public static void main(String[] args){
+
+    }
+}
